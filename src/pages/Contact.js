385f9f113ca70f1733a6f1078e59447b1c1bd655
SazidAhmed <sazidahmed.official@gmail.com>
2022-07-29 22:27:44 +0600

    working on copy to clipboard

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,6 +8,8 @@ function CopyTable() {
     { id: uuidv4(), name: '', email: '', works_on_saturday:'' },
   ]);
 
+  const [isCopied, setIsCopied] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("InputFields", inputFields);
@@ -34,12 +36,28 @@ function CopyTable() {
     setInputFields(values);
   }
 
-  const copyThat = async (accountNumber) => {
-    if (accountNumber)
-      await navigator.clipboard.writeText(accountNumber)
-        .then((res) => {
-          console.log('Number copied successfully', res)
-        })
+  async function copyTextToClipboard(text) {
+    if ('clipboard' in navigator) {
+      return await navigator.clipboard.writeText(text);
+    } else {
+      return document.execCommand('copy', true, text);
+    }
+  }
+
+  // onClick handler function for the copy button
+  const handleCopyClick = () => {
+    // Asynchronously call copyTextToClipboard
+    copyTextToClipboard(copyText)
+      .then(() => {
+        // If successful, update the isCopied state value
+        setIsCopied(true);
+        setTimeout(() => {
+          setIsCopied(false);
+        }, 1500);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   return (
